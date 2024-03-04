648a1496211b0d58c09c6e7daaeef7a195496f11
TheHr <73033570+sycho2417@users.noreply.github.com>
2024-03-04 13:07:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.get('/api/students', async (req, res) => {
   try {
-    const [rows] = await pool.query('SELECT * FROM Students');
+    const [rows] = await pool.query('SELECT * FROM students');
     res.json(rows);
   } catch (err) {
     console.error('Query error:', err.message);
@@ -27,9 +27,9 @@ app.get('/api/students', async (req, res) => {
   }
 });
 
-app.get('/api/students/:RollNo', async (req, res) => {
+app.get('/api/students/:Name', async (req, res) => {
   try {
-    const [rows] = await pool.execute('SELECT * FROM Students WHERE ID = ?', [req.params.ID]);
+    const [rows] = await pool.execute('SELECT * FROM students WHERE ID = ?', [req.params.ID]);
     res.json(rows[0]);
   } catch (err) {
     console.error('Query error:', err.message);
@@ -40,7 +40,7 @@ app.get('/api/students/:RollNo', async (req, res) => {
 app.post('/api/students', async (req, res) => {
   try {
     const { ID , Name} = req.body;
-    const [result] = await pool.execute('INSERT INTO Students (ID, Name) VALUES (?, ?)', [ID, Name]);
+    const [result] = await pool.execute('INSERT INTO students (ID, Name) VALUES (?, ?)', [ID, Name]);
     res.json({ id: result.insertId,ID, Name });
   } catch (err) {
     console.error('Query error:', err.message);
@@ -51,7 +51,7 @@ app.post('/api/students', async (req, res) => {
 app.put('/api/students/:ID', async (req, res) => {
   try {
     const { Name } = req.body;
-    await pool.execute('UPDATE Students SET ID = ? WHERE Name = ?', [ID, req.params.Name]);
+    await pool.execute('UPDATE students SET ID = ? WHERE Name = ?', [ID, req.params.Name]);
     res.json({ID: req.params.Name, ID });
   } catch (err) {
     console.error('Query error:', err.message);
@@ -61,7 +61,7 @@ app.put('/api/students/:ID', async (req, res) => {
 
 app.delete('/api/students/:ID', async (req, res) => {
   try {
-    await pool.execute('DELETE FROM Students WHERE ID = ?', [req.params.ID]);
+    await pool.execute('DELETE FROM students WHERE ID = ?', [req.params.ID]);
     res.json({ ID: req.params.ID, message: 'Student deleted successfully' });
   } catch (err) {
     console.error('Query error:', err.message);
